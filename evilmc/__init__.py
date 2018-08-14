af1790d143522a214a38dfd56b393f645b97f186
Brian Jackson <bjackson@boisestate.edu>
2018-08-14 15:42:58 -0600

    Fixed __init__.py

--- evilmc/__init__.py
+++ evilmc/__init__.py
@@ -1,10 +1,6 @@
 """A python version of the EVIL-MC code"""
 
-<<<<<<< HEAD
-__version__ = '0.20'
-=======
 __version__ = '0.21'
->>>>>>> fixing_eclipse
 __author__ = 'Brian Jackson <bjackson@boisestate.edu>'
 __all__ = ['evilmc']
 
